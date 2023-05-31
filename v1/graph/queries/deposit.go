78e92a0c67c648ac3cf904119986454400120b57
Ceejay <chokey2nv@gmail.com>
2023-05-31 19:31:20 +0100

    moved to deposit - depositor summary

--- v1/graph/queries/deposit.go
+++ v1/graph/queries/deposit.go
@@ -2,8 +2,24 @@ package account
 
 import (
 	"github.com/graphql-go/graphql"
+	payment "github.com/life-entify/account/v1/graph/schemas/payment"
 )
 
+func GetDepositByPersonGroup(resolver graphql.FieldResolveFn) *graphql.Field {
+	return &graphql.Field{
+		Description: "Get Payment By Person ID",
+		Type:        payment.DepositAmountByPersons,
+		Args: graphql.FieldConfigArgument{
+			"skip": &graphql.ArgumentConfig{
+				Type: graphql.Int,
+			},
+			"limit": &graphql.ArgumentConfig{
+				Type: graphql.Int,
+			},
+		},
+		Resolve: resolver,
+	}
+}
 func GetPersonDepositBalance(resolver graphql.FieldResolveFn) *graphql.Field {
 	return &graphql.Field{
 		Description: "Get user deposit balance",
